417829a34306a2f32ca7b434e89727fce642c4b8
Marge <marge.consunji@gmail.com>
2022-10-06 23:21:57 +0800

    Create create-channel command
    - Takes a String option from the command and makes a private text channel with the String option as a name

---
+++ commands/create-channel.js
@@ -0,0 +1,31 @@
+// this command creates a private channel
+require('dotenv').config();
+const { SlashCommandBuilder, ChannelType, PermissionsBitField } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('create-channel')
+		.setDescription('Creates a new private text channel.')
+		.addStringOption(option =>
+			option.setName('channel-name')
+				.setDescription('The name of the channel you want to create')
+				.setRequired(true),
+		),
+	async execute(interaction) {
+		const channelName = interaction.options.getString('channel-name');
+
+		const guildName = await interaction.client.guilds.fetch(process.env.GUILD_ID);
+		const everyoneRole = guildName.roles.everyone;
+		await interaction.guild.channels.create({
+			name: channelName,
+			parent: process.env.CHANNEL_ID,
+			type: ChannelType.GuildText,
+			permissionOverwrites: [
+				{
+					id: everyoneRole,
+					deny: [PermissionsBitField.Flags.ViewChannel],
+				},
+			],
+		});
+	},
+};
